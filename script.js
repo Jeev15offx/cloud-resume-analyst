0f04c20cebb42a1337dafab7a9de34d3721b1bce
Jeev15offx <officialjeevan15@gmail.com>
2026-06-27 16:28:35 +0000

    Phase 3 day 5 configure Nginx reverse proxy

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ async function analyzeResume() {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:5000/analyze",
+            "api/analyze",
             {
                 method: "POST",
 
